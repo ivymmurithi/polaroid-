6f072945c01089cf0b2a289badfacd2b1663f86f
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 12:36:56 +0300

    add forms.py

--- polaroidapp/views.py
+++ polaroidapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
-    return render(request,)
+    return render(request, 'registration/register_form.html')
 
 def index(request):
     return render(request, 'index.html')
