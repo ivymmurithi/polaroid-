6c7d85b5131b5457abdce69fe9c4ad3813cdd07c
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 12:52:34 +0300

    login required

--- polaroidapp/views.py
+++ polaroidapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def register(request):
-    if request.moethpd== 'POST':
+    if request.method== 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
@@ -12,5 +14,6 @@ def register(request):
         form = RegisterForm
     return render(request, 'registration/register_form.html', {'form':form})
 
+@login_required
 def index(request):
     return render(request, 'index.html')
