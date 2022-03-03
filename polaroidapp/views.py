7209f5a211dc1976945106af07556772c0a19b81
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 12:30:31 +0300

    add registration template

--- polaroidapp/views.py
+++ polaroidapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request,)
+
 def index(request):
     return render(request, 'index.html')
