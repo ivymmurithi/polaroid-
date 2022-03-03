849ed1b96be10a1afaac2e83605639138d6db494
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 13:10:19 +0300

    add login.html

--- polaroidapp/urls.py
+++ polaroidapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('register/',views.register,name='register'),
+    path('', include('django.contrib.auth.urls')),
     path('',views.index, name='index')
 ]
