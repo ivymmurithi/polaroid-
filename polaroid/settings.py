c14cc050abbd38834527f62a03bd37c5a119b43b
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 12:56:21 +0300

    add crispy forms to project setting app

--- polaroid/settings.py
+++ polaroid/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polaroidapp'
+    'polaroidapp',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+CRISPY_TEMPLATE_PACK="bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
