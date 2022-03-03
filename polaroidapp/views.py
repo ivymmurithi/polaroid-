bac3909763146a2f0743877f11879cbe36425e00
ivymmurithi <ivymurithi@gmail.com>
2022-03-03 12:50:29 +0300

    registration form setup

--- polaroidapp/views.py
+++ polaroidapp/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'registration/register_form.html')
+    if request.moethpd== 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('')
+    else:
+        form = RegisterForm
+    return render(request, 'registration/register_form.html', {'form':form})
 
 def index(request):
     return render(request, 'index.html')
