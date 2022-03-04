5e053348094dd75563491ac929ac79ac482bc0fa
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 21:01:52 +0300

    homepage design done

--- polaroidapp/views.py
+++ polaroidapp/views.py
@@ -17,3 +17,7 @@ def register(request):
 @login_required
 def index(request):
     return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
