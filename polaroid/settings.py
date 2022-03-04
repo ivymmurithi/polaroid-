2e28f0775b3cb54aacd8fa1786b0596c61a73731
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 13:17:14 +0300

    media path created

--- polaroid/settings.py
+++ polaroid/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,6 +126,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
