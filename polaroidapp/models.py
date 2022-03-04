47e554f9edb4eb1526a4ffa74dc56329386b3671
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 14:02:20 +0300

    make migrations + migrate

--- polaroidapp/models.py
+++ polaroidapp/models.py
@@ -3,34 +3,34 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to = 'profiles/')
-    bio = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
+    bio = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bio
 
 class Image(models.Model):
-    uploaded_image = models.ImageField(upload_to = 'uploads/')
-    image_name = models.CharField(max_length=30)
-    caption = models.TextField()
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    uploaded_image = models.ImageField(upload_to = 'uploads/', null=True)
+    image_name = models.CharField(max_length=30, null=True)
+    caption = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption 
 
 class Comment(models.Model):
-    comment = models.TextField()
-    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.TextField(null=True)
+    image_id = models.ForeignKey(Image,null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment
 
 class Likes(models.Model):
-    likes = models.IntegerField(default=0)
-    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE) 
+    likes = models.IntegerField(default=0, null=True)
+    image_id = models.ForeignKey(Image,null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,null=True, on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.likes
