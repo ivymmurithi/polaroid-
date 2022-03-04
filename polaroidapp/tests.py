0b17d6d9893accef1e3c4a9aebd82934a59f7dd4
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 13:49:59 +0300

    register models to admin

--- polaroidapp/tests.py
+++ polaroidapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+# from .models import Profile,Image,Comment,Likes
 
 # Create your tests here.
+# class ProfileTestClass(TestCase):
+
+#     def setUp(self):
+#         self.user1 = user
+#         self.ivy = Profile(bio = 'Traveller and Violinist at heart')
