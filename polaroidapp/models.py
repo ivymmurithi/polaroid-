1d23a64162917a41068a2145d574dd27b3173c08
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 12:40:32 +0300

    profile model added

--- polaroidapp/models.py
+++ polaroidapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'uploads/')
+    bio = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
