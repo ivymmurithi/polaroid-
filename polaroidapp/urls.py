c9c0d7257e08f0a17c18bf7d8ef0ecf3465a187d
ivymmurithi <ivymurithi@gmail.com>
2022-03-04 13:21:51 +0300

    static route to save images uploaded

--- polaroidapp/urls.py
+++ polaroidapp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/',views.register,name='register'),
     path('', include('django.contrib.auth.urls')),
     path('',views.index, name='index')
 ]
+
+
+"""
+new static route that references the location to the uploaded files
+"""
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
