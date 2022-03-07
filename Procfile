48422b9b88099fc7260f4989c2d67bf01381a150
ivymmurithi <ivymurithi@gmail.com>
2022-03-08 01:11:56 +0300

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn polaroid.wsgi --log-file -
--- polaroid/settings.py
+++ polaroid/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'polaroid.urls'
@@ -126,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 CRISPY_TEMPLATE_PACK="bootstrap4"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -138,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = 'index'
 # LOGOUT_REDIRECT_URL = ''
+
+django_heroku.settings(locals())
