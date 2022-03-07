8d665208008711aaf9c983c8934eef246886a4a2
ivymmurithi <ivymurithi@gmail.com>
2022-03-07 10:40:18 +0300

    test profile, passed

--- polaroidapp/tests.py
+++ polaroidapp/tests.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
-# from .models import Profile,Image,Comment,Likes
+from .models import Profile,Image,Comment
 
 # Create your tests here.
-# class ProfileTestClass(TestCase):
+class ProfileTestClass(TestCase):
 
-#     def setUp(self):
-#         self.user1 = user
-#         self.ivy = Profile(bio = 'Traveller and Violinist at heart')
+    def setUp(self):
+        self.profile1= Profile(bio= 'this is a cat')
+        self.profile1.save_profile()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile1,Profile))
+
+    def test_save_method(self):
+        self.profile1.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile1.save_profile()
+        self.profile1.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) -1)
